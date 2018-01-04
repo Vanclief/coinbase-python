ca21bf449a18ab6441092ac5a6f77bc5187466d3
Franco Valencia <franco.avalencia@gmail.com>
2018-01-04 11:26:18 -0600

    Bumped up version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='gdaxpy',
-    version='0.1.1',
+    version='0.1.2',
     description='Unofficial Python3 library for the Gdax API',
     long_description=readme,
     author='Franco Valencia',
